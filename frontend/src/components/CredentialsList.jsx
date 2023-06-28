1b1f893b7f95b87f95e24ca7ec6fa893df94308f
Cleber Amaral <cleberjamaral@gmail.com>
2023-06-28 17:16:37 -0300

    Delete button

--- frontend/src/components/CredentialsList.jsx
+++ frontend/src/components/CredentialsList.jsx
@@ -17,6 +17,15 @@ function CredentialsList() {
     }
   };
 
+  const handleDelete = async (credential) => {
+    try {
+      await axios.delete(`http://191.36.9.231:5000/api/credentials/${credential.id}`);
+      fetchCredentials();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="container">
       <h2 className="mt-4 mb-4">Credentials List</h2>
@@ -31,6 +40,7 @@ function CredentialsList() {
               <th scope="col">Registration Number</th>
               <th scope="col">User Name</th>
               <th scope="col">Credential</th>
+              <th scope="col">Actions</th>
             </tr>
           </thead>
           <tbody>
@@ -39,6 +49,11 @@ function CredentialsList() {
                 <td>{credential.registration_number}</td>
                 <td>{credential.user_name}</td>
                 <td>{credential.credential}</td>
+                <td>
+                  <button className="btn btn-danger btn-sm" onClick={() => handleDelete(credential)}>
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
