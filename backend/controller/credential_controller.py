fa28cf914f68513d23866bba8aa0356798a889a6
cleberjamaral <cleberjamaral@gmail.com>
2023-07-03 14:40:50 -0300

    Openning gate

--- backend/controller/credential_controller.py
+++ backend/controller/credential_controller.py
@@ -8,6 +8,8 @@ class credential_controller:
         self.view = view
         self.r1d0 = 17 # Wiegand D0 of reader 1
         self.r1d1 = 27 # Wiegand D1 of reader 1
+        self.gate_in = 23 # Open the gate for entrance
+        self.mili = 1000 # 1 ms = 1000 us
         self.is_raspberry_pi = platform.machine().startswith('arm') or platform.machine().startswith('aarch') # Check if running on Raspberry Pi
 
         if self.is_raspberry_pi:
@@ -18,12 +20,25 @@ class credential_controller:
             import controller.wiegand as wiegand             
             import pigpio
             self.wiegand = wiegand
-            self.pi = pigpio.pi()
+            self.pigpio = pigpio
+            self.set_gate_pins()
+            self.open_gate_in()
+            self.pigpio.pi().write(self.gate_in, 0)
         else:
             self.wiegand = None
-            self.pi = None
+            self.pigpio = None
             self.view.display_message(f"Peripherals won't work since it is not a Raspberry Pi! Current platform: "+platform.machine())
-            
+
+    def set_gate_pins(self):
+        self.pigpio.pi().set_mode(self.gate_in, self.pigpio.OUTPUT)
+
+    def open_gate_in(self):
+        #self.pigpio.pulse(1 << self.gate_in, 0, 2000 * self.mili)
+        self.pigpio.pi().write(self.gate_in, 1)
+        import time
+        time.sleep(1)
+        self.pigpio.pi().write(self.gate_in, 0)
+
     def callback(self, bits, value):
         if bits == 34:
             w34 = (value >> 1) & 0xFFFFFFFF
@@ -32,13 +47,14 @@ class credential_controller:
                 self.view.display_message(f"Access granted for credential {hex(w34)[2:]}!")
                 for credential in credentials:
                     self.event_model.insert_event(credential[0], credential[2], "Access granted!")
+                self.open_gate_in()
             else:
                 self.view.display_message(f"Credential {hex(w34)[2:]} not found, access denied!")
                 self.event_model.insert_event(hex(w34)[2:], "User not enrolled", "Access denied!")
 
     def setup_gpio(self):
         if self.is_raspberry_pi:
-            w = self.wiegand.decoder(self.pi, self.r1d0, self.r1d1, self.callback)
+            w = self.wiegand.decoder(self.pigpio.pi(), self.r1d0, self.r1d1, self.callback)
             self.view.display_message(f"GPIO configured on pins: "+str(self.r1d0)+" and "+str(self.r1d1))
             self.view.display_message(f"Waiting for credentials...")
         else:
